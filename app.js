5895f821788ec533d257cc0e6e5909875397d62c
GTgyani206 <gyanendrathakur4135stella@gmail.com>
2024-04-17 21:38:56 +0530

    Minor changes

--- app.js
+++ app.js
@@ -1,62 +1,53 @@
 const del = document.querySelectorAll(".del");
 const complete = document.querySelectorAll(".complete");
-const add = document.querySelector(".task-btn"); // Changed selector to match the button's class
-const taskInput = document.querySelector(".task-text"); // Changed selector to match the input's class
+const add = document.querySelector("#button-addon2");
+const taskInput = document.getElementById("taskInput");
 const edit = document.querySelectorAll(".edit");
 const retry = document.querySelectorAll(".retry");
 let text = "";
-
 for (delBtn of del) {
   delBtn.addEventListener("click", () => {
     console.log("delete");
     // delete the item
   });
 }
-
 for (completeBtn of complete) {
   completeBtn.addEventListener("click", () => {
     console.log("complete");
     // mark the item as complete
   });
 }
-
 add.addEventListener("click", () => {
-  const task = taskInput.value; // Changed to use the correct variable name and selector
+  const task = taskInput.value; // Changed add.innertext to taskInput.value to get the input value
   console.log(task);
   text = task;
   // text = task; // You can use this if you need to store the task text in a variable
   // Add your code to add the task to a list or perform any other action with the task text
 });
-
 for (editBtn of edit) {
   editBtn.addEventListener("click", () => {
     console.log("edit");
     // edit the item
   });
 }
-
 for (retryBtn of retry) {
   retryBtn.addEventListener("click", () => {
     console.log("retry");
     // mark the item as retry
   });
 }
-
-let quoteUrl = "https://api.quotable.io/random"; // Corrected variable name
-const Quote = document.querySelector(".quote"); // Corrected selector name
-
+let qouteUrl = "https://api.quotable.io/random";
+const Qoute = document.querySelector(".qoute");
 function getQuote() {
-  axios.get(quoteUrl).then((response) => {
-    let quote = response.data.content; // Corrected variable name
+  axios.get(qouteUrl).then((response) => {
+    let qoute = response.data.content;
     let author = response.data.author;
-    let finalQuote = `<p>${quote}</p> <p>-${author}</p>`; // Corrected variable name
-
+    let finalqoute = `<p>${qoute}</p> <p>-${author}</p>`;
     const p = document.createElement("p");
-    p.innerHTML = finalQuote;
-    Quote.appendChild(p);
+    p.innerHTML = finalqoute;
+    Qoute.appendChild(p);
   });
 }
-
 function getTime() {
   let time = new Date();
   let hours = time.getHours();
@@ -74,7 +65,5 @@ function getTime() {
   const timeDiv = document.querySelector(".time");
   timeDiv.innerHTML = finalTime;
 }
-
 setInterval(getTime, 1000);
-
 getQuote();
